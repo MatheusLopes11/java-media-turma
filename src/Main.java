b5a3abbea63435a6b89f9fca1e6d4d01d519ea1c
Matheus <matheus.lopes6@aluno.cps.sp.gov.br>
2026-07-03 19:39:20 -0300

    v2

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,18 @@ public class Main {
 
         Scanner entrada = new Scanner(System.in);
 
-        double somaNotas = 0;
-        double nota;
-        int quantidade = 0;
+        double media;
+
+        double somaMedias = 0;
+        int quantidadeMedias = 0;
+
+        int idAluno = 1;
+
+        double maiorMedia = 0;
+        double menorMedia = 10;
+
+        int quantidadeAprovados = 0;
+        int quantidadeReprovados = 0;
 
         System.out.println("--------------------------------");
         System.out.println("CALCULADORA MÉDIA GERAL DA TURMA");
@@ -20,25 +29,41 @@ public class Main {
 
         do{
 
-            System.out.println("Digite a média: ");
-            nota = entrada.nextDouble();
+            System.out.printf("Digite a média do %dº aluno: \n", idAluno);
+            media = entrada.nextDouble();
             System.out.println();
 
-            if (nota >= 0 && nota <= 10){
+            if (media >= 0 && media <= 10){
 
-                somaNotas += nota;
-                quantidade++;
+                if (media > maiorMedia) maiorMedia = media;
+                if (media < menorMedia) menorMedia = media;
+
+                if (media >= 6) quantidadeAprovados++;
+                if (media < 6) quantidadeReprovados++;
+
+                somaMedias += media;
+                quantidadeMedias++;
+                idAluno++;
             }
-            else if (nota > 10) System.out.println("Média inválida!\n");
+            else if (media > 10) System.out.println("Média inválida!\n");
 
         }
-        while(nota >= 0);
+        while(media >= 0);
 
-        if (quantidade > 0) {
+        if (quantidadeMedias > 0) {
 
-            double media = somaNotas / quantidade;
+            double mediaFinal = somaMedias / quantidadeMedias;
 
-            System.out.printf("Média geral: %.2f", media);
+            System.out.println("----------");
+            System.out.println("RESULTADOS");
+            System.out.println("----------");
+            System.out.printf("Média geral: %.2f %n", mediaFinal);
+            System.out.printf("Maior média: %.2f %n", maiorMedia);
+            System.out.printf("Menor média: %.2f %n", menorMedia);
+            System.out.println("--------------------");
+            System.out.printf("Quantidade de alunos: %d %n", quantidadeMedias);
+            System.out.printf("Quantidade de Aprovados: %d %n", quantidadeAprovados);
+            System.out.printf("Quantidade de Reprovados: %d %n", quantidadeReprovados);
 
         }
         else{
